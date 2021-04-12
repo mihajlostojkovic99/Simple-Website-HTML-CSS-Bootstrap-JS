af745a28495a48d63b6859840b2b7550721e5399
Mihajlo Stojkovic <70921912+mihajlostojkovic99@users.noreply.github.com>
2021-04-12 11:40:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,14 +6,34 @@ function proveri() {
     var tel = document.mojaforma.tel.value
     
     if (ime=="" || prezime=="" || tip == "" || br == "" || tel=="") {
-        alertify.warning("Unesite sva polja!")
+        alertify.warning("Please fill all the fields!")
         return
     }
 
     var telRegEx = /^(((064)|(065)|(063)|(061))\/\d{4}-\d{3})$/
     if (!telRegEx.test(tel)) {
-        alertify.error('Greska za telefon')
+        alertify.error('Wrong phone number format')
         return
     }
 
+    var brNocenja = parseInt(br) 
+    var racun = 0
+    var status = false
+    if (tip == "Hotel" && brNocenja <= 2) {
+        racun = brNocenja*2000
+        status = true
+    } else if (tip == "Villa" && brNocenja <= 5) {
+        racun = brNocenja*1500
+        status = true
+    } else if (tip == "Village house" && brNocenja <= 10) {
+        racun = brNocenja*1000
+        status = true
+    }
+
+    if (!status) {
+        alertify.error("That number of nights is not available at this accomodation")
+    } else {
+        alertify.success("Your reservation is successful! Dear " + ime + " " + prezime + ",<br/>Your total ammount to pay is " + racun + 
+            ".<br/>We will contact you via your phone number " + tel + " for further information.<br/>Have a nice stay!<br/>(Click this message to dismiss it)", 0)
+    }
 }
